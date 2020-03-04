dc01ad9817b7a3443d639c00f08fe27aad4c7091
Ubuntu <ubuntu@ip-172-31-82-213.ec2.internal>
2020-03-04 17:55:15 +0000

    Lodown corrections

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -4,12 +4,29 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
+
 var factorial = function(n) {
+//Create a base case (if n === 0, return 1)
+if (n === 0) {
+  return 1;
+// if n is a negative number, return null
+} else if (Math.sign(n) === -1) {
+  return null;
+}
+//Create recursive case
+return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+//Create base case (if array.length === 0)
+  if(array.length === 0) {
+    return 0; 
+  }
+//Create recursion case
+// return the first item in the array plus sum enacted on the rest of the array
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
@@ -19,25 +36,83 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+//Create a base case
+// if n === 0, return true
+  if(n === 0) {
+    return true;
+//else if n===1, return false
+  } else if (n === 1) {
+    return false;
+  }
+//Create a recursive case for positive numbers
+  // if n is a positive integer
+  if (Math.sign(n) === 1) {
+    // return the function invoked on n - 2
+    return isEven(n - 2);  
+  // else if n is a negative integer
+  } else if(Math.sign(n) === -1) {
+    // return the function invoked on n + 2
+    return isEven(n + 2);  
+  }
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  // Create base case
+  if(n === 0) {
+    return 0;
+  } 
+  // Create a recursive case
+  // if n is negative
+  if(Math.sign(n) === -1) {
+    //return n + 1  +
+    // sumBelow invoked on the negative number
+    return n + 1 + sumBelow(n + 1);
+  // if n is postive
+  } else if(Math.sign(n) === 1) {
+    // return n - 1 + sumBelow invoked on the positive number
+    return n - 1 + sumBelow(n - 1);
+  }
 };
 
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
+
 var range = function(x, y) {
-};
+  // Create base case
+  if (x === y || x + 1 === y || x - 1 === y) {
+    return []; // ???
+  } 
+  //Create recursive case
+  if (x < y) {
+    let numbers = range(x , y - 1);
+    numbers.push(y - 1);
+    return numbers;
+  } else if (x > y) {
+    let numbers = range(x, y + 1);
+    numbers.push(y + 1);
+    return numbers;
+  }
+}
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64.  Here, 8 is the base and 2 is the exponent.
 // Example:  exponent(4,3);  // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
+
 var exponent = function(base, exp) {
+   if (exp === 0) {
+    return 1;
+  } 
+  // recursive case
+  if (exp > 0) {
+    return base * exponent(base, exp - 1);
+  } else if (exp < 0) {
+    return  1 / (base * exponent(base, -exp -1));
+  }
 };
 
 // 8. Determine if a number is a power of two.
@@ -45,15 +120,38 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  return n == 1 ? true : (n < 1 ? false : powerOfTwo(n/2));
 };
 
 // 9. Write a function that accepts a string a reverses it.
 var reverse = function(string) {
+  // base case
+  if (string === "") {
+    return string;
+  // recursive case
+  } else {
+    return reverse(string.substr(1)) + string.charAt(0);
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  //eliminate spaces and convert to lowercase
+  let baseString = string.trim().toLowerCase();
+  //base case
+  if (baseString.length <= 1) {
+    return true;
+  }
+  // recursive case
+  if(baseString[0] === baseString[baseString.length - 1]) {
+    // return palindrome invoked on the baseString with the first and last letters removed
+    return palindrome(baseString.slice(1).slice(baseString.length - 1));
+  } else {
+    return false;
+  }
 };
+
+
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
